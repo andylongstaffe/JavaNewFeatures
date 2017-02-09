80445e42010d88996dc05abefaba26a8c392fa86
Andy Longstaffe <andylongstaffe@gmail.com>
2017-02-09 23:20:05 +0000

    Methodrefs continued.

--- src/com/hollywood/java8/methodreferences/ComparatorExample.java
+++ src/com/hollywood/java8/methodreferences/ComparatorExample.java
@@ -5,26 +5,8 @@ package com.hollywood.java8.methodreferences;
  */
 public class ComparatorExample {
 
-    class Person {
-        private int height;
-
-        public Person(int height) {
-            this.height = height;
-        }
-
-        public int getHeight() {
-            return height;
-        }
-
-        public int compareByAge(Person a, Person b) {
-            return new Integer(a.height).compareTo(b.height);
-        }
-    }
-
     public static void main(String[] args) {
-        Person[] persons = {new Person(), new Person()};
-
-
+        Person[] persons = {new Person(10), new Person(15)};
     }
 
 
---
+++ src/com/hollywood/java8/methodreferences/Person.java
@@ -0,0 +1,21 @@
+package com.hollywood.java8.methodreferences;
+
+/**
+ * Created by andylongstaffe on 09/02/2017.
+ */
+public
+class Person {
+    private int height;
+
+    public Person(int height) {
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int compareByAge(Person a, Person b) {
+        return new Integer(a.height).compareTo(b.height);
+    }
+}
